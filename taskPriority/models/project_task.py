dd27e01a2ed81d83a87b21941e2de16ae21a6410
Judal1 <guillaumevillers@hotmail.com>
2025-02-14 08:39:20 +0100

    try color

--- taskPriority/models/project_task.py
+++ taskPriority/models/project_task.py
@@ -1,10 +1,15 @@
-from odoo import models,fields;
+from odoo import models,fields,api;
 
 class Task(models.Model):
     _inherit = 'project.task'
 
     importance_id = fields.Many2one('priority.importance', string='Importance')
     complexity_id = fields.Many2one('priority.complexity', string='Complexity')
+    complexity_color = fields.Char(string='Complexity Color', compute='_compute_complexity_color')
 
 
+    @api.depends('importance_id')
+    def _compute_complexity_color(self):
+        for r in self:
+            r.complexity_color = r.complexity_id.color;
 
